3e98dbf2d4c1ac06fc4ba8940c6c92707febfd5e
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-06-24 17:31:27 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { presentatorList } from './presentatorList.ts';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default App;
