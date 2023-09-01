8d9c7dfd830793f9d8a1bc56ac1a5fd25df7c90f
oratake <smith.shimomura@gmail.com>
2023-09-01 09:23:07 +0900

    feat: ボタン類の位置調整

--- src/ShuffleList.tsx
+++ src/ShuffleList.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import presentatorList from './presentatorList.ts';
 import {
+  AppShell,
+  Group,
   Table,
   Title,
   Button,
@@ -70,43 +72,52 @@ const ShuffleList: React.FC = () => {
   );
 
   return (
-    <div>
-      <Title order={1}>プログラミング部 有志LT会 おしながき</Title>
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Th>リベ名</Table.Th>
-            <Table.Th>発表タイトル</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {list.map((item) => (
-            <Table.Tr key={item.id}>
-              <Table.Td>{item.id}</Table.Td>
-              <Table.Td>{item.member}</Table.Td>
-              <Table.Td>{item.title}</Table.Td>
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+    >
+      <AppShell.Header>
+        <Title order={1}>プログラミング部 有志LT会 おしながき</Title>
+      </AppShell.Header>
+      <AppShell.Main>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>ID</Table.Th>
+              <Table.Th>リベ名</Table.Th>
+              <Table.Th>発表タイトル</Table.Th>
             </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
-      <Button
-        variant="gradient"
-        gradient={{ from: 'blue', to: 'cyan', deg: 30 }}
-        onClick={isShuffling ? stopShuffle : startShuffle}
-        disabled={isLocked ? true : false}
-      >
-        {isShuffling ? 'ストップ' : 'スタート'}
-      </Button>
-      <Switch
-        checked={isLocked}
-        onChange={(event) => setLocked(event.currentTarget.checked)}
-        size="lg"
-        color="red"
-        onLabel={onLabelIcon}
-        offLabel={offLabelIcon}
-      />
-    </div>
+          </Table.Thead>
+          <Table.Tbody>
+            {list.map((item) => (
+              <Table.Tr key={item.id}>
+                <Table.Td>{item.id}</Table.Td>
+                <Table.Td>{item.member}</Table.Td>
+                <Table.Td>{item.title}</Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+        <Group padding="md" justify="space-between">
+          <Button
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan', deg: 30 }}
+            onClick={isShuffling ? stopShuffle : startShuffle}
+            disabled={isLocked ? true : false}
+          >
+            {isShuffling ? 'ストップ' : 'スタート'}
+          </Button>
+          <Switch
+            checked={isLocked}
+            onChange={(event) => setLocked(event.currentTarget.checked)}
+            size="lg"
+            color="red"
+            onLabel={onLabelIcon}
+            offLabel={offLabelIcon}
+          />
+        </Group>
+      </AppShell.Main>
+    </AppShell>
   );
 };
 
